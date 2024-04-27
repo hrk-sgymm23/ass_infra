05381a291c2667a6eb7450a43268e5b6f7c9ba34
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-04-27 22:27:16 +0900

    add oai setting

--- enviroments/stg/web/s3.tf
+++ enviroments/stg/web/s3.tf
@@ -1,8 +1,5 @@
-# locals {
-#     common_name = "ass-stg"
-# }
-
 module "ass_web_s3_stg" {
   source      = "../../../modules/s3"
   common_name = "${var.common_name}-${var.environment}"
+  oai_identifiers = module.ass_web_cf_stg.oai_identifiers
 }
