d6aa4054da69d380d8072fab2ce9f8bc3b91a2a5
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-05-08 21:41:03 +0900

    feat: add ecr module

---
+++ modules/ecr/main.tf
@@ -0,0 +1,17 @@
+resource "aws_ecr_repository" "main" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = var.repository_name
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+  policy = file("${path.module}/ecr_lifecycle_policy.json")
+}
