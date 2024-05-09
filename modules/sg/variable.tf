84bf9251272ef886178786697f33544a7f8fc48c
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2024-05-09 21:05:22 +0900

    feat: add security group module not apply

---
+++ modules/sg/variable.tf
@@ -0,0 +1,19 @@
+variable "security_group_name" {
+  type = string
+  description = "SecurityGroup Name"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "VPC id"
+}
+
+variable "port" {
+  type = number
+  description = "Permit ConnectionPort"
+}
+
+variable "cidr_blocks" {
+  type = list(string)
+  description = "CIDR Block List "
+}
